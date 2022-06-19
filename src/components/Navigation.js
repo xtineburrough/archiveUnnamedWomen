fdf19f00632485f5fe6916965b822111ead0f244
devdesign21 <devdesign21@protonmail.com>
2022-06-19 16:17:21 -0500

    navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,19 +2,23 @@ import "./Navigation.css";
 
 export default function Navigation() {
   return (
-    <ul>
-      <li>
-        <a href="#">An Archive of Unnamed Women</a>
-      </li>
+    <div className="nav">
+      <ul>
+        <li>
+          <a href="#">
+            An Archive of <span style={{ color: "white" }}>Unnamed</span> Women
+          </a>
+        </li>
 
-      <li style={{ float: "right" }}>
-        <a href="#contact">Contact</a>
-      </li>
-      <li style={{ float: "right" }}>
-        <a className="active" href="#about">
-          About
-        </a>
-      </li>
-    </ul>
+        <li style={{ float: "right" }}>
+          <a href="#contact">All Collection</a>
+        </li>
+        <li style={{ float: "right" }}>
+          <a className="active" href="#about">
+            Home
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
