a2d386609ca8f6eab84ea3bdc8fc0dac25d54f19
devdesign21 <devdesign21@protonmail.com>
2022-07-12 00:23:45 -0500

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,29 @@ export default function Main({
   idName,
 }) {
   let showCollection = (currentCollection) => {
+    if (currentCollection == "Home") {
+      console.log("RETRNUNG TO HOME");
+      return;
+    }
+    const colCopy = [...currentCollection];
     let content = [];
+    for (let i = 0; i < colCopy.length; i++) {
+      const randomNum = Math.floor(Math.random() * colCopy.length);
+      const itemData = colCopy[randomNum];
+
+      content.push(
+        <div key={itemData.id}>
+          <NewModal
+            className="modalPrint"
+            itemData={itemData}
+            quoteList={quoteList}
+            collectionName={collectionName}
+            idName={idName}
+          />
+        </div>
+      );
+      colCopy.splice(randomNum, 1);
+    }
     currentCollection.map((itemData, index) =>
       content.push(
         <div key={index}>
