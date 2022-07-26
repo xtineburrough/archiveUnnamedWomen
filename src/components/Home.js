c5a8fcb015abee227b6413735339c5ddda441f50
xtine burrough <xtineburrough@gmail.com>
2022-07-25 22:26:43 -0500

    update home with image and type style

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,87 +7,54 @@ export default function About() {
         <p id="visit">
           View an edition of this project by choosing a collection from the pull-down menu in the upper right corner.
         </p>
-        <h1>About</h1>
-        <p>
-          <em>An Archive of Unnamed Women</em> is a speculative remix that
-          recovers photographs of unidentified women in digital photography
-          collections. This project highlights a critical discussion about
-          gendered power relations in archived visual materials that are saved
-          as part of a digital object&#8217;s metadata. First initiated with the
-          New York Public Library digital collection, including photographs
-          archived in the Schomburg Center for Research in Black Culture, this
-          project has been supported at The University of Texas at Dallas for an
-          edition with the Comer Collection. Visitors who search our
-          re-presentation of unnamed women are presented with their photographs
-          and, upon clicking for more information, a juxtaposed description
-          drawn from a parallel collection of women&#8217;s writing about women.{" "}
-        </p>
+       <div className="type">
+		<h2>An Archive of Unnamed Women</h2>
+			
+			<p><em>An Archive of Unnamed Women</em>is a speculative remix that recovers photographs of unidentified women in digital photography collections. This project highlights a critical discussion about gendered power relations in archived visual materials that are saved as part of a digital object&#8217;s metadata.
+			</p>
+	</div>
+	
 
-        <p>
-          At conferences, gatherings, and festivals, our feminist remix workshop
-          includes an opportunity for participants to select a quote for the
-          woman they are most drawn to, and print a postcard in remembrance
-          while simultaneously engaging a conversation about how and why these
-          images have been shifted to the background of the archive through
-          algorithms and gendered power relations writ large. We hang a
-          hand-made quilt with a screen in the centerpiece, whereupon attendees
-          who have signed their names in our guestbook will see that they have
-          been included into the fabric of the project—our archive of named
-          participants.{" "}
-        </p>
+		<div className="type">
+			<p>Visitors who search<em>An Archive of Unnamed Women</em>are presented with their photographs and, upon clicking for more information, a juxtaposed description drawn from a parallel collection of women&#8217;s writing about women.</p>
+		</div>		
+				
+	<div className="type">
+	<img src="http://missconceptions.net/unnamed-women/img/nypl-sample.png" class="responsive">
+	</div>
+	<div className="type">			
+      <h2>NYPL and Other Collections Coming Soon</h2>
+      <p>First initiated with the New York Public Library digital collection, including photographs archived in the Schomburg Center for Research in Black Culture, this project has been supported at The University of Texas at Dallas for an edition with the Comer Collection.</p>
+	</div>
 
-        <p>
-          Included in this project is a button to identify a women. The marker
-          of success would be the undoing of the project itself. If you can
-          identify a person included in our image set, press the Identify button
-          and fill in the form that follows. We will put you in touch with the
-          archivist who can update the photograph descriptions and add names to
-          the faces of women from the past.{" "}
-        </p>
+	<div className="type">
+		<h2>Workshops</h2>
+ <p>At conferences, gatherings, and festivals, our feminist remix workshop includes an opportunity for participants to select a quote for the woman they are most drawn to, and print a postcard in remembrance while simultaneously engaging a conversation about how and why these images have been shifted to the background of the archive through algorithms and gendered power relations writ large. We hang a hand-made quilt with a screen in the centerpiece, whereupon attendees who have signed their names in our guestbook will see that they have been included into the fabric of the project—our archive of named participants. </p>
+		</div>
 
-        <h2>Project Credits</h2>
-        <p>
-          xtine burrough is a media artist who planted the first seeds for this
-          project in a chapter of{" "}
-          <em>Foundations of Digital Art and Design.</em> She oversaw the design
-          and development of the digital project and collaborated with Sabrina
-          Jian Starnaman on the format and direction of workshops they deliver
-          together.
-        </p>
+	<div className="type">
+		<h2>Help Us Unmake This Project: Identify a Woman</h2>
+ <p>Included in this project is a button that links to a form we are using for people who can identify a women. One marker of success would be the undoing of the project. If you can identify a person included in our image set, press the Identify button and fill in the form that follows. We will put you in touch with the archivist who can update the photograph descriptions and add names to the faces of women from the past.  </p>
+		</div>
+	
+	<div className="type">
+<h2>Project Credits</h2>
+ <p>xtine burrough is a media artist who planted the first seeds for this project in a chapter of<em>Foundations of Digital Art and Design.</em>She oversaw the design and development of the digital project and collaborated with Sabrina Jian Starnaman on the format and direction of workshops they deliver together.</p>
+	  
+<p>Sabrina Jian Starnaman is a literature professor and a Buddhist Chaplain. She oversaw the development of the database of literary quotes that appear in the NYPL edition, assisted in quilting, and co-leads workshops. </p>
+	  
+ <p>Alyssa Yates is a UT Dallas Arts & Humanities alumni who culled the NYPL digital collection, one image at a time, to find the 422 images in our archive. She is also a master quilter. In addition to generating the dataset of unnamed women in the NYPL edition, she also directed the making of the quilt and assisted in gathering quotes.</p>
 
-        <p>
-          Sabrina Jian Starnaman is a literature professor and a Buddhist
-          Chaplain. She oversaw the development of the database of literary
-          quotes that appear in the NYPL edition, assisted in quilting, and
-          co-leads workshops.{" "}
-        </p>
+	  <p>Al Madireddy is a UT Dallas Computer Science and Engineering alumni who developed the back-end programming for the first version of this project (2018).</p>
 
-        <p>
-          Alyssa Yates is a UT Dallas Arts & Humanities alumni who culled the
-          NYPL digital collection, one image at a time, to find the 422 images
-          in our archive. She is also a master quilter. In addition to
-          generating the dataset of unnamed women in the NYPL edition, she also
-          directed the making of the quilt and assisted in gathering quotes.
-        </p>
+	<p>Nicolas Nguyen is a UT Dallas Business student with experience in web development. He revised and redeveloped the back-end programming for the current version of the project (2022).  </p>
+		</div>
 
-        <p>
-          Al Madireddy is a UT Dallas Computer Science and Engineering alumni
-          who developed the back-end programming for the first version of this
-          project (2018).
-        </p>
-
-        <p>
-          Nicolas Nguyen is a UT Dallas Business student with experience in web
-          development. He revised and redeveloped the back-end programming for
-          the current version of the project (2022).{" "}
-        </p>
-
-        <h2>Sponsors</h2>
-        <p>
-          We appreciate the kind sponsorship of an artist microgrant from the
-          Nasher Sculpture Center in 2019 and a UT Dallas 2022 aXh
-          &#8220;Uncertain Futures&#8221; grant (2022).
-        </p>
+	<div className="type">
+<h2>Sponsors</h2>
+ <p>We appreciate the kind sponsorship of a 2019 artist microgrant from the Nasher Sculpture Center and a 2022 UT Dallas aXh &#8220;Uncertain Futures&#8221; grant.	  
+	  </p>
+		</div>
       </div>
     </div>
   );
