e712b211e2ecee7b03feb09fc61968942924a14b
xtine burrough <xtineburrough@gmail.com>
2022-07-25 22:31:35 -0500

    fixed img tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ export default function About() {
 		</div>		
 				
 	<div className="type">
-	<img src="http://missconceptions.net/unnamed-women/img/nypl-sample.png" class="responsive">
+	<img src="http://missconceptions.net/unnamed-women/img/nypl-sample.png" className="responsive" alt="sample" />
 	</div>
 	<div className="type">			
       <h2>NYPL and Other Collections Coming Soon</h2>
